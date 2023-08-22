a61ffda7efbdc6988d5d10dd21d705eb1ecd8837
LFabreu <lfabreu1707@gmail.com>
2023-08-22 20:21:30 -0300

    aula_14-javascript

---
+++ aula_14/test.js
@@ -0,0 +1,108 @@
+// comentário
+
+/**
+ * comentario de multiplas linhas
+ * 
+ * 
+ * 
+ */
+
+// tipos de dados
+/**
+ * Number => int float NaN
+ * String => texto
+ * boolean => true false
+ * null => vazio
+ * undefined => não tem definição
+ */
+
+/**
+ * converter  tipos
+ * De numero para string
+ * var.toString()
+ * 
+ * De string para numero
+ * parseInt(var)
+ */
+
+/**
+ * operadores aritméticos
+ * + soma
+ * - subtração
+ * / divisão
+ * * multiplicação
+ * % resto da divisão
+ * ** potencia
+ * 
+ */
+
+/**
+ * operadores relacionais
+ * < menor que
+ * > maior que
+ * >= maior ou igual
+ * <= menor ou igual
+ * == igualdade
+ * === se os tipos são iguais
+ * != diferente 
+ */
+
+/**
+ * operadores logicos
+ * '! Não -Negação
+ * '&& E - conjunção
+ * '|| ou - disjunção
+ */
+
+/**
+ * variaveis
+ * var = vc pode redefinir - escopo global
+ * let = vc não pode redefinir - escopo local
+ * const = constante, uma informação que vc não irá mudar
+ */
+
+/**
+ * array = listas
+ * objetos = dicionario
+ * json => java script object notation {chave: valor}
+ */
+
+/**
+ * para saber algum tipo de dado
+ * 
+ * !typeof()
+ */
+
+/**
+ * funções
+ * function nome_da_função(parametros){
+ *     comandos
+ *     return alguma_coisa
+ *      
+ * }
+ * 
+ */
+
+/**
+ * condicionais
+ * if (condição1){
+ *    plano A
+ * }else if (condição2){
+ *    plano B
+ * }else{
+ *   plano resto
+ * }
+ */
+
+/**
+ * loop
+ * for (let i = 0; i < 10; i++){
+ *     o que tem que ser repetido
+ * }
+ */
+
+document.getElementById('titulo').innerHTML = "bom dia"
+
+document.write('lucas')
+
+console.log('teste 213')
