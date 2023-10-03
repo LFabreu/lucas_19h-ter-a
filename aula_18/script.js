864269a0be398754b209a45f1ec637d553ada294
Lucas <lfabreu1707@gmail.com>
2023-10-03 13:42:51 -0300

    tentativa e mt erro

--- aula_18/script.js
+++ aula_18/script.js
@@ -1,31 +1,30 @@
-var n1=parseInt(prompt("digite a 1ª Nota "));
-var n2=parseInt(prompt("digite a 2ª Nota "));
-var n3=parseInt(prompt("digite a 3ª Nota "));
+var nota1=prompt("digite a Nota n°1");
+var nota2=prompt("digite a Nota n°2");
+var nota3=prompt("digite a Nota n°3");
+var nome1=prompt("digite o seu nome");
 
-let soma = n1 + n2 + n3
-let resp = soma / 3
-
-const nome = document.getElementById("nome")
-const média = document.getElementById("nota");
-const result = document.getElementById("resultado");
-
-function Passou() {
-    if (resp >= 5) {
-        result.innerHTMl = 'aprovado'   
-        
-    }
-    else if (resp <= 4) {
-        result.innerText = "Reprovou"
-    }
-}
-média.innerHTML = resp
+document.getElementById("nome")
+document.getElementById("média");
+document.getElementById("resultado");
 
 
-
-function mudar(){
-    nome.innerHTML = prompt("digite o seu nome")
+function calcular(){
+    return resp
 }
 
-Passou()
+function Passou(resultado) {
+    soma = nota1 + nota2 + nota3
 
-mudar();
+    resp = soma/3
+
+    if resp > 5{
+        resultado.innerText("passou")
+    }
+    if resp < 5{
+        resultado.innerText("Reprovou")
+    }
+}
+
+function nome(){
+    nome.innerText(nome1)
+}
--- aula_19/java.js
+++ aula_19/java.js
@@ -2,7 +2,28 @@ const n1 = document.getElementById("n1");
 const n2 = document.getElementById("n2");
 const n3 = document.getElementById("n3");
 
-soma = n1 + n2 + n3
+let soma = n1 + n2 + n3
+let resp = soma/3
 
-resp = soma / 3
+const nome = document.getElementById("nome")
+const média = document.getElementById("nota");
+const result = document.getElementById("resultado");
 
+function Passou() {
+    if (resp >= 5) {
+        result.innerHTMl = 'aprovado'   
+        
+    }
+    else if (resp <= 4) {
+        result.innerText = "Reprovou"
+    }
+}
+média.innerHTML = resp
+
+function mudar(){
+    nome.innerHTML = nome
+}
+
+Passou()
+
+mudar();
