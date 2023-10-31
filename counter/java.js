08809593fd525f72db9bdae5fa0b7fd1f1a40f69
Lucas <lfabreu1707@gmail.com>
2023-10-31 18:04:20 -0300

    tentativa aula 20

---
+++ counter/java.js
@@ -0,0 +1,33 @@
+const value = document.getElementById('value');
+const plusButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+
+const updateValue = () => {
+    value.innerHTML = count;
+}
+
+let count = 0;
+let intervalId = 0;
+
+plusButton.addEventListener('mousedowm', () => {
+    intervalId =  setInterval(() => {
+        count +=1;
+        updateValue();
+    },100);
+})
+
+document.addEventListener('mouseup', () => clearInterval(intervalId));
+
+minusButton.addEventListener('mousedowm', () => {
+    intervalId =  setInterval(() => {
+        count -=1;
+        updateValue();
+    },100);
+})
+
+
+resetButton.addEventListener('click', () => {
+    count = 0;
+    updateValue();
+});
