b9119eb46364992158557a98f8f9a88249b172dd
LFabreu <lfabreu1707@gmail.com>
2023-11-28 20:26:55 -0300

    aula 24 - semi-finalizando

--- aula_24/script.js
+++ aula_24/script.js
@@ -1,13 +1,43 @@
 const URL_BASE = 'https://restcountries.com/v3.1/name/'
 
+const FORM = document.querySelector('.conteudo-pesquisa')
+const ENTRADA = document.querySelector('.search')
+const NOME = document.querySelector('.nome-oficial')
+const BANDEIRA = document.querySelector('.bandeira')
+const TERRITORIO = document.querySelector('.territorio')
+const CAPITAL = document.querySelector('.capital')
+const CONTINENTE = document.querySelector('.continente')
+const SUB_REGIAO = document.querySelector('.sub-regiao')
+const SIGLA = document.querySelector('.sigla')
+const POPULAÇÂO = document.querySelector('.Idioma')
+
+
 let fetch_country = async(country) =>{
-    const response = await fetch(`${URL_BASE}${country}`)
+    const response = await fetch(URL_BASE + country)
     if (response.status == 200) {
-        const INFO = response.json
+        const INFO = response.json()
+        return INFO
     }
 }
 
-fetch_country('Brazil')
+const render_country = async(countries) => {
+    const info = await fetch_country(countries)
+    if (info) {
+        NOME.innerHTML = (info[0].name.official)
+        SIGLA.innerHTML = (info[0].fifa)
+        CONTINENTE.innerHTML = (info[0].region)
+        CAPITAL.innerHTML = (info[0].capital)
+        TERRITORIO.innerHTML = (info[0].area)
+        SUB_REGIAO.innerHTML = (info[0].subregion)
+        POPULAÇÂO.innerHTML = (info[0].population)
+        BANDEIRA.src = (info[0].flags['svg'])
+        console.log(info)
+    }
+}
+
+
+
+render_country('Portugal')
 
 /**
  * o que trazer de informação
